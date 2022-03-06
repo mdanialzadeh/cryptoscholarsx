7f9769816733a7a7202806b2dd292ab8816b7581
mdanialzadeh <mdanialzadeh10@gmail.com>
2022-03-06 11:48:06 -0500

    Updates

---
+++ precache-manifest.518260f6dfa392f937d6781398c4ab9f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b55ddfa6e48c529b594e1aa4038805",
+    "url": "/cryptoscholarsx/index.html"
+  },
+  {
+    "revision": "5f05c9c362db0b5749e6",
+    "url": "/cryptoscholarsx/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "e60ac87a0a4a6b01b827",
+    "url": "/cryptoscholarsx/static/css/main.69894e7b.chunk.css"
+  },
+  {
+    "revision": "5f05c9c362db0b5749e6",
+    "url": "/cryptoscholarsx/static/js/2.efbcae94.chunk.js"
+  },
+  {
+    "revision": "e60ac87a0a4a6b01b827",
+    "url": "/cryptoscholarsx/static/js/main.6eb27049.chunk.js"
+  },
+  {
+    "revision": "2c76dc9e0115fd179bf5",
+    "url": "/cryptoscholarsx/static/js/runtime~main.179acac1.js"
+  },
+  {
+    "revision": "0be0c6ef866125fd9fc0fd00d5ee8a35",
+    "url": "/cryptoscholarsx/static/media/Jade.0be0c6ef.jpg"
+  },
+  {
+    "revision": "d3b413aab4514729713b5de7e4a397b4",
+    "url": "/cryptoscholarsx/static/media/King.d3b413aa.jpg"
+  },
+  {
+    "revision": "37d67297e62619435655be228017db0e",
+    "url": "/cryptoscholarsx/static/media/Knight.37d67297.jpg"
+  },
+  {
+    "revision": "b8b95d790360e525ea7c19f92c51e86a",
+    "url": "/cryptoscholarsx/static/media/Queen.b8b95d79.jpg"
+  },
+  {
+    "revision": "7b95cb30b7400bd90807166b5201bfbd",
+    "url": "/cryptoscholarsx/static/media/Ruby.7b95cb30.jpg"
+  },
+  {
+    "revision": "115ad6811a8f1f25c23c16174fd4652e",
+    "url": "/cryptoscholarsx/static/media/Saphire.115ad681.jpg"
+  },
+  {
+    "revision": "56f7a571beaf6358d427d2adb281b946",
+    "url": "/cryptoscholarsx/static/media/b_bishop.56f7a571.png"
+  },
+  {
+    "revision": "3d3c55d9f94bababb754e1dca411c427",
+    "url": "/cryptoscholarsx/static/media/b_king.3d3c55d9.png"
+  },
+  {
+    "revision": "ef53bf4929b4d31f9ecba5440941dd88",
+    "url": "/cryptoscholarsx/static/media/b_knight.ef53bf49.png"
+  },
+  {
+    "revision": "263e35be5a9e86e3daaf9d554453bdf0",
+    "url": "/cryptoscholarsx/static/media/b_pawn.263e35be.png"
+  },
+  {
+    "revision": "c288c39c08ca9775c0a61d394202609e",
+    "url": "/cryptoscholarsx/static/media/b_queen.c288c39c.png"
+  },
+  {
+    "revision": "961ae27ab5893d90d68218ab578a3142",
+    "url": "/cryptoscholarsx/static/media/b_rook.961ae27a.png"
+  },
+  {
+    "revision": "ceb838c516e7677156b135c5d631ef12",
+    "url": "/cryptoscholarsx/static/media/bannerx.ceb838c5.png"
+  },
+  {
+    "revision": "ace68760456b65e68031ced7f058a72f",
+    "url": "/cryptoscholarsx/static/media/chess_board.ace68760.png"
+  },
+  {
+    "revision": "c3c71bf678c7f946830815813367995f",
+    "url": "/cryptoscholarsx/static/media/w_bishop.c3c71bf6.png"
+  },
+  {
+    "revision": "72b40fd46b32e93bead92c3997990c73",
+    "url": "/cryptoscholarsx/static/media/w_king.72b40fd4.png"
+  },
+  {
+    "revision": "df1f4abc56cd161400c79336f84fa829",
+    "url": "/cryptoscholarsx/static/media/w_knight.df1f4abc.png"
+  },
+  {
+    "revision": "d5b58d2e83a38c9bce870aedb090948d",
+    "url": "/cryptoscholarsx/static/media/w_pawn.d5b58d2e.png"
+  },
+  {
+    "revision": "7bd9d7eeba087f0388ea4d365df1ef03",
+    "url": "/cryptoscholarsx/static/media/w_queen.7bd9d7ee.png"
+  },
+  {
+    "revision": "8934d9c8f90c48cb5c1be2a01a966090",
+    "url": "/cryptoscholarsx/static/media/w_rook.8934d9c8.png"
+  }
+]);
